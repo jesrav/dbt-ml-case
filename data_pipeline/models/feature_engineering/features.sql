dcd1c8111dff7cdab61d60bed80d25bf5224a35f
jesrav <jesravnbol@hotmail.com>
2021-12-11 05:58:04 +0000

    added timestamps and lagged feature

--- data_pipeline/models/feature_engineering/features.sql
+++ data_pipeline/models/feature_engineering/features.sql
@@ -2,6 +2,31 @@
 
 {{ config(materialized='table') }}
 
-select *
+select "Location",
+    "WindGustDir",
+    "WindDir3pm",
+    "RainToday",
+    "RainTomorrow",
+    "MinTemp",
+    "MaxTemp",
+    "Rainfall",
+    "Evaporation",    
+    "Sunshine",
+    "Pressure9am",
+    "Pressure3pm",
+    "Temp9am",
+    "Temp3pm",
+    "WindGustSpeed",
+    "WindSpeed9am",
+    "WindSpeed3pm",
+    "Humidity9am",
+    "Humidity3pm",
+    "Cloud9am",
+    "Cloud3pm", 
+    
+    lag(rainfall, 1, 0) over (partition by location order by location, date) as rainyesterday, 
+
+    CURRENT_TIMESTAMP() as created_at
+
 from {{ ref('stg_weather_aus') }}
 
